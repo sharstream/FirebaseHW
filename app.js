1702e7aef989f7a94045e93c63f18bb4266350b3
David Perez <david.perez@us.abb.com>
2018-03-25 22:03:23 -0400

    working with table using jquery and moment.js

--- app.js
+++ app.js
@@ -17,60 +17,98 @@ $(document).ready(function(){
     // Create a variable to reference the database.
     var database = firebase.database();
 
+    var trains = [];
 
-    $("#submit").on("click", function (event) {
-        // prevent page from refreshing when form tries to submit itself
+    function displayTable(ele) {
+        appendTableColumn($('table'), ele);
+    }
 
-        event.preventDefault();
+    function makeTable(container, data) {
+        var table = $("table");
+        $.each(data, function (rowIndex, r) {
+            var row = $("<tr/>");
+            $.each(r, function (colIndex, c) {
+                row.append($("<t" + (rowIndex == 0 ? "h" : "d") + "/>").text(c));
+            });
+            table.append(row);
+        });
+        return container.append(table);
+    }
 
-        // Capture user inputs and store them into variables
-        var name = $("#name").val().trim();
-        var destination = $("#destination").val().trim();
-        var time = $("#time").val().trim();
-        var frequency = $("#frequency").val().trim();
-        var trains = new Array ();
+    function appendTableColumn(table, rowData) {
+        var lastRow = $('<tr/>').appendTo(table.find('tbody:last'));
+        $.each(rowData, function (colIndex, c) {
+            lastRow.append($('<td/>').text(c));
+        });
 
-        // Console log each of the user inputs to confirm we are receiving them
-        console.log(name);
-        console.log(destination);
-        console.log(time);
-        console.log(frequency);
+        return lastRow;
+    }
 
+    function retrieveTravels() {
+        // debugger
+        var rootRef = database.ref();
+        var urlRef = rootRef.once("value", function(snapshot){
+            snapshot.forEach(function (child) {
+                var trainTable = makeTable($(document.table), 
+                        [[child.val().name, child.val().destination, child.val().frequency, child.val().nextArrive, child.val().away]]);
+                trains.push([child.val().name, child.val().destination, child.val().frequency, child.val().nextArrive, child.val().away]);
+            });
+        });
+    }
+
+    function databasePush(name, destination, frequency, nextArrive, away) {
         database.ref().push({
             name: name,
             destination: destination,
-            time: time,
-            frequency: frequency
+            frequency: frequency,
+            nextArrive: nextArrive,
+            away: away
         });
+    }
+    
+    $("#submit").on("click", function (event) {
+        // prevent page from refreshing when form tries to submit itself
+        // debugger
+        event.preventDefault();
+        
+        // Capture user inputs and store them into variables
+
+        var name = $("#name").val().trim();
+        var destination = $("#destination").val().trim();
+
+        var firstTime = moment($("#firstTime").val().trim(), "hh:mm A").format("LT");
+        var frequency = moment($("#frequency").val().trim(), "m").format("m");
+
+        var nextArrive = "3:00 PM";
+        var away = "2";
+    // nextArrive = moment(firstTime).add(frequency, 'hh:mm A').format("HH:mm A");
+    // away = moment(nextArrive).diff(firstTime, 'HH');
+
+        databasePush(name, destination, frequency, nextArrive, away);
+        var obj;
         database.ref().limitToLast(1).on("child_added", function(snapshot){
             console.log(snapshot.val());
             if (snapshot.child("name").exists() &&
                     snapshot.child("destination").exists() &&
-                    snapshot.child("time").exists() &&
-                    snapshot.child("frequency").exists())
+                    snapshot.child("frequency").exists() &&
+                    snapshot.child("nextArrive").exists() &&
+                    snapshot.child("away").exists())
             {
 
-                var obj = {
-                    name: snapshot.val().name,
-                    destination: snapshot.val().destination,
-                    time: snapshot.val().time,
-                    frequency: snapshot.val().frequency
-                };
+                obj = [
+                    snapshot.val().name,
+                    snapshot.val().destination,
+                    snapshot.val().frequency,
+                    snapshot.val().nextArrive,
+                    snapshot.val().away
+                ]
                 trains.push(obj);
-                console.log('' + trains);
             }
         });
-        for (var i = 0; i < trains.length; i++){
-
-            console.log(trains[i]);
-
-            $("tbody").append("<td>" + trains[i].name + "</td>");
-            $("tbody").append("<td>" + trains[i].destination + "</td>");
-            $("tbody").append("<td>" + trains[i].time + "</td>");
-            $("tbody").append("<td>" + trains[i].frequency + "</td>");
-        };
+        displayTable(obj);
     });
 
+    retrieveTravels();
 
 });
 
